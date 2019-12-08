28d7ac4858a9422a5ad17e40993c5573b639476d
maibroadcast <azizannordin81@gmail.com>
2019-12-09 07:15:35 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,30 @@ import {AllCommunityModules} from "@ag-grid-community/all-modules";
 export class AppComponent  {
 
     columnDefs = [
-        {field: 'make' },
-        {field: 'model' },
-        {field: 'price'}
+        {field: 'NAMA PENGELUAR NOTIS' },
+        {field: 'JENIS NOTIS' },
+        {field: 'JUMLAH NOTIS'}
     ];
 
     rowData = [
-        { make: 'Toyota', model: 'Celica', price: 35000 },
-        { make: 'Ford', model: 'Mondeo', price: 32000 },
-        { make: 'Porsche', model: 'Boxter', price: 72000 }
+        { NAMA PENGELUAR NOTIS: 'PP AZIZAN', 'JENIS NOTIS': 'Celica', 'JUMLAH NOTIS' ice: 35000 },
+        { KPPPP RIDHUAN: 'Ford', model: 'Mondeo', price: 32000 },
+        { PP K. ADLIZAM: 'Porsche', model: 'Boxter', price: 72000 }
+		{ AB29 ZULHILMI: 'Porsche', model: 'Boxter', price: 72000 }
+		{ AB29 POK NIK : 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP29 RUSAIRI : 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP29 HAKEMI  : 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 BOB: 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 AHHHH: 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 JADSJHAD: 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 JADSJHADAM: 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 JADSJHADDLIZAM: 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 JADSJHADIZAM: 'Porsche', model: 'Boxter', price: 72000 }
+		{ PP K. ADLIZAM: 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 JADSJHADLIZAM: 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 JADSJHADZAM: 'Porsche', model: 'Boxter', price: 72000 }
+		{ KP19 JADSJHADZAM: 'Porsche', model: 'Boxter', price: 72000 }
+		{ 
     ];
 
     modules = AllCommunityModules;
